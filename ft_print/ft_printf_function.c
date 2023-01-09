01f92a87de3dce024517d37c96263de0991dd21d
Wonjin Lee <wonjilee@cx2r1s2.42seoul.kr>
2023-01-09 19:50:58 +0900

    pf

--- ft_print/ft_printf_function.c
+++ ft_print/ft_printf_function.c
@@ -6,7 +6,7 @@
 /*   By: wonjilee <wonjilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 19:01:40 by wonjilee          #+#    #+#             */
-/*   Updated: 2022/12/28 15:15:32 by wonjilee         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:48:45 by wonjilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,54 +88,4 @@ int	start_print(t_list *converts, va_list ap)
 		converts = converts->next;
 	}
 	return (len);
-}
-
-int	print_convert(t_convert *text, va_list ap)
-{
-	if (text->index == PLAIN)
-		return (print_plain(text));
-	else if (text->index == CHAR)
-		return (print_char(text, va_arg(ap, int)));
-	else if (text->index == STR)
-		return (print_str(text, va_arg(ap, char *)));
-	else if (text->index == PTR)
-		return (print_ptr(text, va_arg(ap, void *)));
-	else if (text->index == DEC)
-		return (print_dec(text, va_arg(ap, int)));
-	else if (text->index == IDEC)
-		return (print_idec(text, va_arg(ap, int)));
-	else if (text->index == UDEC)
-		return (print_udec(text, va_arg(ap, int)));
-	else if (text->index == LHEX)
-		return (print_lhex(text, va_arg(ap, int)));
-	else if (text->index == UHEX)
-		return (print_uhex(text, va_arg(ap, int)));
-	else if (text->index == PCENT)
-		return (print_pcent(text));
-	return (-1);
-}
-
-int	check_flags(char **locate)
-{
-	locate++;
-	if (*locate == 'c')
-		return (CHAR);
-	else if (*locate == 's')
-		return (STR);
-	else if (*locate == 'p')
-		return (PTR);
-	else if (*locate == 'd')
-		return (DEC);
-	else if (*locate == 'i')
-		return (IDEC);
-	else if (*locate == 'u')
-		return (UDEC);
-	else if (*locate == 'x')
-		return (LHEX);
-	else if (*locate == 'X')
-		return (UHEX);
-	else if (*locate == '%')
-		return (PCENT);
-	else
-		return (-1);
 }
--- ft_print/ft_printf_function3.c
+++ ft_print/ft_printf_function3.c
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_function2.c                              :+:      :+:    :+:   */
+/*   ft_printf_function3.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wonjilee <wonjilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 21:05:56 by wonjilee          #+#    #+#             */
-/*   Updated: 2023/01/09 19:47:55 by wonjilee         ###   ########.fr       */
+/*   Created: 2023/01/09 19:47:18 by wonjilee          #+#    #+#             */
+/*   Updated: 2023/01/09 19:47:54 by wonjilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-//출력할 문자열의 길이 리턴
-int	print_plain(t_convert *text)
-{
-	int	len;
 
-	len = 0;
-
-
-	return (len);
-}
-
-int	print_char(t_convert *text, char c)
+int	print_udec(t_convert *text, int num)
 {
 }
 
-int	print_str(t_convert *text, char *str)
+int	print_lhex(t_convert *text, int num)
 {
 }
 
-int	print_dec(t_convert *text, int num)
+int	print_udex(t_convert *text, int num)
 {
 }
 
-int	print_idec(t_convert *text, int num)
+int	print_pcent(t_convert *text)
 {
 }

1b700dccc0506a05aee95b7555e8e5761d81d317
Wonjin Lee <wonjilee@c1r7s1.42seoul.kr>
2023-07-25 20:03:22 +0900

    philo

--- philosophers/ft_print.c
+++ philosophers/ft_print.c
@@ -6,7 +6,7 @@
 /*   By: wonjilee <wonjilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 19:11:08 by wonjilee          #+#    #+#             */
-/*   Updated: 2023/07/25 19:59:52 by wonjilee         ###   ########.fr       */
+/*   Updated: 2023/07/25 20:02:53 by wonjilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,14 @@ void	thread_print(t_data *data, int action, int philo)
 	now.tv_usec = now.tv_usec - data->t_start;
 	pthread_mutex_lock(&data->sys[PRINT]);
 	if (action == EAT)
-	{
-	}
+		printf("%d %d is eating", now.tv_usec, philo);
 	else if (action == SLEEP)
-	{
-	}
+		printf("%d %d is sleeping", now.tv_usec, philo);
 	else if (action == THINK)
-	{
-	}
+		printf("%d %d is thinking", now.tv_usec, philo);
 	else if (action == FORK)
-	{
-	}
+		printf("%d %d has taken fork", now.tv_usec, philo);
 	else if (action == DIE)
-	{
-	}
+		printf("%d %d died", now.tv_usec, philo);
 	pthread_mutex_unlock(&data->sys[PRINT]);
 }

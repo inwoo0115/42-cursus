58e5782f7707f3c5d5aebf8320d09bff922b54b2
Wonjin Lee <wonjilee@c1r7s1.42seoul.kr>
2023-07-25 20:00:04 +0900

    philo

--- philosophers/ft_print.c
+++ philosophers/ft_print.c
@@ -6,14 +6,18 @@
 /*   By: wonjilee <wonjilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 19:11:08 by wonjilee          #+#    #+#             */
-/*   Updated: 2023/07/25 19:28:45 by wonjilee         ###   ########.fr       */
+/*   Updated: 2023/07/25 19:59:52 by wonjilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	thread_print(t_data *data, int action, int time, int philo)
+void	thread_print(t_data *data, int action, int philo)
 {
+	struct timeval	now;
+
+	gettimeofday(&now, NULL);
+	now.tv_usec = now.tv_usec - data->t_start;
 	pthread_mutex_lock(&data->sys[PRINT]);
 	if (action == EAT)
 	{

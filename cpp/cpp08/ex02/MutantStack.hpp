303168303f96860de42c12fef14a190b9b29ed30
Wonjin Lee <wonjilee@c1r7s7.42seoul.kr>
2024-03-12 22:18:04 +0900

    cpp08

--- cpp/cpp08/ex02/MutantStack.hpp
+++ cpp/cpp08/ex02/MutantStack.hpp
@@ -1,7 +1,6 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <iterator>
 #include <stack>
 
 template <typename T>
@@ -14,9 +13,9 @@ class MutantStack : public std::stack<T> {
     return (*this);
   };
   ~MutantStack(){};
-  typedef typename std::stack<T>::contianer_type::iterator iterator;
+  typedef typename std::stack<T>::container_type::iterator iterator;
   typedef
-      typename std::stack<T>::contianer_type::reverse_iterator reverse_iterator;
+      typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 
   iterator begin() { return this->c.begin(); }
   iterator end() { return this->c.end(); }
